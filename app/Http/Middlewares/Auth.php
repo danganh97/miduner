b39fb685610d30521dda318d760c9c63265ebc15
Vo Dang Anh <danganh.dev@gmail.com>
2019-09-05 17:34:03 +0700

    comment

--- app/Http/Middlewares/Auth.php
+++ app/Http/Middlewares/Auth.php
@@ -6,6 +6,11 @@ use App\Main\Middleware;
 
 class Auth extends Middleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @return mixed
+     */
     public function handle($callback, $action, $params)
     {
         return parent::next($callback, $action, $params);
