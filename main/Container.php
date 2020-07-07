bb038454ca317a6826ac54fd8c8c9cbfeb471177
Vo Dang Anh <danganh.dev@gmail.com>
2020-07-07 13:26:57 +0700

    add comment

--- main/Container.php
+++ main/Container.php
@@ -4,12 +4,28 @@ namespace Main;
 
 class Container
 {
+    /**
+     * Storage saving registry variables
+     * @var array $storage
+     */
     private $storage = [];
 
+    /**
+     * Storage saving bindings objects
+     * @var array $bindings
+     */
     private $bindings;
 
+    /**
+     * The instance of the container
+     * @var self $instance
+     */
     private static $instance;
 
+    /**
+     * Get instance of the container
+     * @return self
+     */
     public static function getInstance()
     {
         if (!self::$instance) {
@@ -18,11 +34,34 @@ class Container
         return self::$instance;
     }
 
+    /**
+     *
+     * Make a entity
+     * @param string $entity
+     * @return mixed
+     */
     public function make($entity)
     {
         return $this->__get($entity);
     }
 
+    /**
+     *
+     * Make a entity
+     * @param string $entity
+     * @return mixed
+     */
+    public function resolve($entity)
+    {
+        return $this->make($entity);
+    }
+
+    /**
+     * Register a concrete to abstract
+     * @param string $entity
+     * @param mixed $singleton
+     * @return void
+     */
     public function singleton($entity, $singleton)
     {
         if (is_callable($singleton)) {
@@ -53,11 +92,17 @@ class Container
         return $this->bindings;
     }
 
+    /**
+     * Setter
+     */
     public function __set($name, $value)
     {
         $this->storage[$name] = !isset($this->storage[$name]) ? $value : $this->storage[$name];
     }
 
+    /**
+     * Getter
+     */
     public function __get($name)
     {
         return isset($this->storage[$name]) ? $this->storage[$name] : null;
